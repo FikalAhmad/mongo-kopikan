dc35a353a177fc4389409d00e38ab3076a8f49d8
FikalAhmad <fikaalm26@gmail.com>
2023-08-07 07:31:19 +0700

    chores(core): index, database

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize({
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
-  host: process.env.DB_HOST,
+const db = new Sequelize("kopikan_db", "root", "", {
+  host: "localhost",
   dialect: "mysql",
 });
 
